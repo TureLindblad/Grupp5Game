72dbfdff305936841880e36913ee11f56d6449ee
TureLindbladDesktop <ture.gustaf.lindblad@gmail.com>
2024-04-04 20:22:41 +0200

    repair collision

--- Collision.cs
+++ Collision.cs
@@ -17,21 +17,19 @@ namespace Grupp5Game
                             enemy.HealthBar.CurrentHealth -= ((projectile.MagicDamage - enemy.MagicArmor)
                                                                + (projectile.MagicDamage / 2));
                             _ = projectile.ApplyProjectileEffect(enemy);
-                            return true;
                         }
                         if (projectile is Arrow)
                         {
                             enemy.HealthBar.CurrentHealth -= ((projectile.PhysDamage - enemy.PhysArmor)
                                                                + (projectile.PhysDamage / 2));
                             _ = projectile.ApplyProjectileEffect(enemy);
-                            return true;
                         }
                     }
+
+                    projectile.DamageAnimation(enemy);
+
+                    return true;
                 }
-
-                projectile.DamageAnimation(enemy);
-
-                return true;
             }
 
             return false;
